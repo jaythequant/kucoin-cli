9affdea880ce47ec24e583ea87c43f031c36aeba
jvanlandingham <96921246+jaythequant@users.noreply.github.com>
2022-05-16 23:47:22 -0400

    added date range parser

--- utils/_helpers.py
+++ utils/_helpers.py
@@ -1,13 +1,59 @@
-from datetime import datetime
+import datetime as dt
+import timedelta
+import re
+import math
 
 
-def parse_date(date_string):
+# Map of interval options for kline data
+interval_map = {"min": "minutes", "hour": "hours", "day": "days"}
+# Map of number of minutes in hours, days, weeks
+minutes_map = {"min": 1, "hour": 60, "day": 1440, "week": 10_080}
+
+
+def _parse_date(date_string):
+    """Parse date string to datetime object"""
     if ":" in date_string:
-        dt_obj = datetime.strptime(
+        dt_obj = dt.datetime.strptime(
             date_string, "%Y-%m-%d %H:%M:%S"
         )
     else:
-        dt_obj = datetime.strptime(
+        dt_obj = dt.datetime.strptime(
             date_string, "%Y-%m-%d"
         )
     return dt_obj
+
+
+def _parse_interval(begin, end, interval) -> list:
+    """
+    Parse timedelta into number of calls required to API
+    
+    :return call_range: Returns a list of tuples containing beginning
+        and ending date ranges. These ranges will appropriately paganate
+        the kline data returned from get_kline_data call in Client
+    """
+    max_bars = 1500
+
+    _, num, inc = re.split('(\d+)', interval)  # Parse interval
+    delt = timedelta.Timedelta(end - begin)
+    # Calculate total number of bars needed
+    bars = getattr(delt.total, interval_map[inc]) / int(num)
+
+    if bars < max_bars:
+        return [(begin, end)]
+    else:
+        call_ranges = []
+        # Calculate number of calls and split to int calls and
+        # residual float call
+        f, i = math.modf(bars / max_bars) 
+        n = 1500 * minutes_map[inc] * int(num)
+        begin = begin
+        end = end
+        for _ in range(int(i)):
+            begin = end - dt.timedelta(minutes=n)
+            call_ranges.append((begin, end))
+            end = begin
+        n = int(n * f)
+        end = begin
+        begin = end - dt.timedelta(minutes=n)
+        call_ranges.append((begin, end))
+        return call_ranges
