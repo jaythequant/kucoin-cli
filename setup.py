c3c4efcd220854f60eac576c64ac591e595a8e16
jvanlandingham <96921246+jaythequant@users.noreply.github.com>
2022-05-17 18:37:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.0.5"
+version = "0.0.6"
 
 setuptools.setup(
     name="kucoin-cli",
