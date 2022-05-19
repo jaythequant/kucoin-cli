60f05c60a9252215f1e018c1e23eb92c82d04fe0
jvanlandingham <96921246+jaythequant@users.noreply.github.com>
2022-05-19 18:28:40 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.0.8"
+version = "0.0.9"
 
 setuptools.setup(
     name="kucoin-cli",
