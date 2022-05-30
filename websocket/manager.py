18419926cf52d5f0822d8b210691238279886d0a
jvanlandingham <96921246+jaythequant@users.noreply.github.com>
2022-05-30 12:20:46 -0400

    Add files via upload

---
+++ websocket/manager.py
@@ -0,0 +1,49 @@
+import json
+import websockets
+import asyncio
+import time
+import logging
+import random
+
+logging.getLogger(__name__)
+
+
+async def connection_manager(socket, timeout=5000):
+    """
+    Error handling and keep alive for socket server
+
+    :param timeout: Timeout parameter for keep-alive messages
+
+    :return: JSON object with subscription details
+    """
+    keep_waiting = True
+    try:
+        while keep_waiting:
+            print(1)
+            last_ping = time.time()
+            if time.time() - last_ping > timeout:
+                await _send_ping(socket)
+            try:
+                evt = await asyncio.wait_for(socket.recv(), timeout=timeout)
+            except asyncio.TimeoutError:
+                logging.debug(f"No message in {timeout} seconds")
+                await _send_ping(socket)
+            except asyncio.CancelledError:
+                logging.debug("Cancelled error thrown")
+                await _send_ping(socket)
+            else:
+                # Handle any data manipulations that need to occur
+                resp = json.loads(evt)
+                return resp
+    except websockets.ConnectionClosed:
+        keep_waiting = False
+        await connection_manager(socket, timeout)
+    except Exception as e:
+        keep_waiting = False
+        await connection_manager(socket, timeout)
+
+
+async def _send_ping(socket):
+    """Sends a ping message to socket connection"""
+    msg = {"id": str(int(time.time() * 1000)), "type": "ping"}
+    await socket.send(json.dumps(msg))
