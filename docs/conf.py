e9273cde47a94b4d6d764734732c5fe999e26e3d
jvanlandingham <96921246+jaythequant@users.noreply.github.com>
2022-08-15 23:48:53 -0400

    edited sphinx requirements file

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, James VanLandingham'
 author = 'James VanLandingham'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.2.7'
 
 
 # -- General configuration ---------------------------------------------------
